5795d1e70797252b1c566c0adaae60c2564870ca
Austin Kammerer <austin4191@gmail.com>
2021-11-10 14:30:23 -0600

    include AddItem component

---
+++ src/components/AddItem/AddItem.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+function AddItem(props) {
+  const [description, setDescription] = useState("");
+  const [url, setUrl] = useState("");
+  const dispatch = useDispatch();
+
+  const addItem = (e) => {
+    e.preventDefault();
+
+    // send input data to saga
+    dispatch({
+      type: "ADD_ITEM",
+      payload: { description, url },
+    });
+
+    // Clear inputs immediately for now
+    setDescription("");
+    setUrl("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={addItem}>
+        <input
+          type="text"
+          placeholder="description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <input
+          type="text"
+          placeholder="Image Url"
+          onChange={(e) => setUrl(e.target.value)}
+          value={url}
+        />
+        <button type="submit">Add Item</button>
+      </form>
+    </div>
+  );
+}
