b87f4bec0cce2739e6668f189629cf5fe6596d39
Austin Kammerer <austin4191@gmail.com>
2021-11-10 14:10:21 -0600

    server POST handler

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,32 +1,52 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
-
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.sendStatus(200); // For testing only, can be removed
 });
 
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post("/", rejectUnauthenticated, (req, res) => {
   // endpoint functionality
+  const newItem = req.body;
+  console.log(newItem);
+
+  const query = `
+  INSERT INTO "item" ("description", "image_url", "user_id") VALUES ($1, $2, $3);`;
+
+  const values = [newItem.description, newItem.image_url, req.user.id];
+
+  pool
+    .query(query, values)
+    .then((result) => {
+      console.log("POST SUCCESS");
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log("POST ERROR", error);
+      res.sendStatus(500);
+    });
 });
 
 /**
  * Delete an item if it's something the logged in user added
  */
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // endpoint functionality
 });
 
 /**
  * Update an item if it's something the logged in user added
  */
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // endpoint functionality
 });
 
@@ -34,14 +54,14 @@ router.put('/:id', (req, res) => {
  * Return all users along with the total number of items
  * they have added to the shelf
  */
-router.get('/count', (req, res) => {
+router.get("/count", (req, res) => {
   // endpoint functionality
 });
 
 /**
  * Return a specific item by id
  */
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // endpoint functionality
 });
 
