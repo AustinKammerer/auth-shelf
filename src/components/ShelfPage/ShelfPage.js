7e082cf74cd0fae48df0b8a7d3cdfe657dc4f7f0
Matt Dow <matt.dow.vo@gmail.com>
2021-11-10 13:52:11 -0600

    Added fetchItems saga

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 function ShelfPage() {
+  const dispatch = useDispatch();
+  const items = useSelector(store => store.items);
+
+  useEffect(() => {
+    dispatch({type: 'FETCH_ITEMS'})
+  }, [dispatch])
+
   return (
     <div className="container">
       <h2>Shelf</h2>
